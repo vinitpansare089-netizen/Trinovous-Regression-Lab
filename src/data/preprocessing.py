234b7363e02efc9528bd934497422435f9e781ad
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-03-26 23:52:37 +0530

    revoke changes in file

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -1,22 +1,16 @@
-# ## preprocessing done through this module
-# import pandas as pd
-# def preprocess(df):
-#     print("Preprossing data")
+## preprocessing done through this module
+import pandas as pd
+def preprocess(df):
+    print("Preprossing data")
 
-#     #drop null values
-#     df = df.dropna()
+    #drop null values
+    df = df.dropna()
 
-#     #convert categorical columns to numeric
-#     df = pd.get_dummies(df, drop_first=True)
-    
-#     print("preprossing done")
-#     return df
-
-
-    # Encode categorical
+    #convert categorical columns to numeric
     df = pd.get_dummies(df, drop_first=True)
     
-    # Split features & target
-    X = df.drop("G3", axis=1)
-    y = df["G3"]
+    print("preprossing done")
+    return df
+
+
     

9d61dba6a23921183e4263bb49dead3ea3d45f80
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-03-26 23:56:13 +0530

    changes in evaluate.py

--- src/evaluation/evaluate.py
+++ src/evaluation/evaluate.py
@@ -1,11 +1,22 @@
-##model evaluating done in this module
-import numpy as np
-from sklearn.metrics import mean_squared_error
+# ##model evaluating done in this module
+# import numpy as np
+# from sklearn.metrics import mean_squared_error
 
-def evaluate(model, X, y):
-    print("Evaluating Model")
-    predictions = model.predict(X)
-    mse = mean_squared_error(y, predictions)
+# def evaluate(model, X, y):
+#     print("Evaluating Model")
+#     predictions = model.predict(X)
+#     mse = mean_squared_error(y, predictions)
+#     rmse = np.sqrt(mse)
+#     print("Evaluated Model")
+#     return mse, rmse
+
+from sklearn.metrics import mean_squared_error
+import numpy as np
+
+def evaluate_model(model, X_test, y_test):
+    predictions = model.predict(X_test)
+    
+    mse = mean_squared_error(y_test, predictions)
     rmse = np.sqrt(mse)
-    print("Evaluated Model")
+    
     return mse, rmse

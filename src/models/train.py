b0c8a8b3bf196e87e4ce646737e779caebb38373
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-03-26 23:55:05 +0530

    train_model fuction built

--- src/models/train.py
+++ src/models/train.py
@@ -1,8 +1,15 @@
+# from sklearn.linear_model import LinearRegression
+
+# def train_model(X, y):
+#     print("training model...")
+#     model = LinearRegression()
+#     model.fit(X, y)
+#     print("Model Trained")
+#     return model
+
 from sklearn.linear_model import LinearRegression
 
-def train_model(X, y):
-    print("training model...")
+def train_model(X_train, y_train):
     model = LinearRegression()
-    model.fit(X, y)
-    print("Model Trained")
+    model.fit(X_train, y_train)
     return model

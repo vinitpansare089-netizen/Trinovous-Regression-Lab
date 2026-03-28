80c446992c28172d6cb08d9e24ee1f9e298ce930
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-03-29 04:24:37 +0530

    save the model as a file

--- src/models/train.py
+++ src/models/train.py
@@ -1,15 +1,13 @@
-# from sklearn.linear_model import LinearRegression
-
-# def train_model(X, y):
-#     print("training model...")
-#     model = LinearRegression()
-#     model.fit(X, y)
-#     print("Model Trained")
-#     return model
-
 from sklearn.linear_model import LinearRegression
+import joblib
 
-def train_model(X_train, y_train):
+def train_model(X, y):
+    print("training model...")
     model = LinearRegression()
-    model.fit(X_train, y_train)
+    model.fit(X, y)
+    #Save the model in as a file
+    joblib.dump(model, "vinit_model.pkl")
+
+    print("Model Trained")
     return model
+

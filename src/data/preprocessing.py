a54a6e97bbfdb0848cec5e2c50658191374794ba
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-03-29 03:58:55 +0530

    run main.oy fixes import errors

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -1,39 +1,39 @@
-# ## preprocessing done through this module
-# import pandas as pd
-# def preprocess(df):
-#     print("Preprossing data")
-
-#     #drop null values
-#     df = df.dropna()
-
-#     #convert categorical columns to numeric
-#     df = pd.get_dummies(df, drop_first=True)
-    
-#     print("preprossing done")
-#     return df
-
+## preprocessing done through this module
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+def preprocess(df):
+    print("Preprossing data")
 
-def preprocess_data(df):
-    
-    # Encode categorical
+    #drop null values
+    df = df.dropna()
+
+    #convert categorical columns to numeric
     df = pd.get_dummies(df, drop_first=True)
     
-    # Split features & target
-    X = df.drop("G3", axis=1)
-    y = df["G3"]
+    print("preprossing done")
+    return df
+
+# import pandas as pd
+# from sklearn.model_selection import train_test_split
+# from sklearn.preprocessing import StandardScaler
+
+# def preprocess_data(df):
     
-    # Train-test split
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42
-    )
+#     # Encode categorical
+#     df = pd.get_dummies(df, drop_first=True)
     
-    # Scaling
-    scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
+#     # Split features & target
+#     X = df.drop("G3", axis=1)
+#     y = df["G3"]
     
-    return X_train, X_test, y_train, y_test
+#     # Train-test split
+#     X_train, X_test, y_train, y_test = train_test_split(
+#         X, y, test_size=0.2, random_state=42
+#     )
+    
+#     # Scaling
+#     scaler = StandardScaler()
+#     X_train = scaler.fit_transform(X_train)
+#     X_test = scaler.transform(X_test)
+    
+#     return X_train, X_test, y_train, y_test
     

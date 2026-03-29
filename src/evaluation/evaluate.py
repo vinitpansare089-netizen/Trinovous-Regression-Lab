9c8e0f510d60660d95e57b41832036bb77090529
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-03-30 04:18:26 +0530

    maintain flow

--- src/evaluation/evaluate.py
+++ src/evaluation/evaluate.py
@@ -2,9 +2,9 @@
 import numpy as np
 from sklearn.metrics import mean_squared_error
 
-def evaluate_model(model, X_test, y_test):
+def evaluate_model(best_model, X_test, y_test):
     print("Evaluating Model")
-    predictions = model.predict(X_test)
+    predictions = best_model.predict(X_test)
     mse = mean_squared_error(y_test, predictions)
     rmse = np.sqrt(mse)
     print("Evaluated Model")

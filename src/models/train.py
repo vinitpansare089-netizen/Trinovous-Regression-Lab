5a7b4eb7860a9d8fd31c64c40f372e2681dea562
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-03-30 04:17:43 +0530

    add grid in train.py

--- src/models/train.py
+++ src/models/train.py
@@ -1,17 +1,29 @@
 from sklearn.tree import DecisionTreeRegressor
 import joblib
+from sklearn.model_selection import GridSearchCV
 
 def train_model(X_train, y_train):
-    print("training model...")
+    print("training model with Tuning...")
 
-    model = DecisionTreeRegressor(max_depth=5, min_samples_split=10)
-    model.fit(X_train, y_train)
+    model = DecisionTreeRegressor(random_state=42)
+
+    params = {
+        "max_depth" : [3,5,7,10],
+        "min_samples_split" : [2, 5, 10]
+    }
+
+    grid = GridSearchCV(model, params, cv=5, scoring="neg_mean_squared_error")
+    grid.fit(X_train, y_train)
+
+    best_model = grid.best_estimator_
+    print("Params:" , grid.best_params_)
     
+    return best_model
     #Save the model in as a file
-    print("Loading...model file")
-    joblib.dump(model, "models/vinit_model.pkl")
-    print("loaded.")
+    # print("Loading...model file")
+    # joblib.dump(model, "models/vinit_model.pkl")
+    # print("loaded.")
 
-    print("Model Trained")
-    return model
+    # print("Model Trained")
+    # return model
 

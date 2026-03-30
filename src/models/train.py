bbce2c84a2544f1cdbe623af20f4aec129c22a3d
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-03-31 00:45:41 +0530

    train on decision tree

--- src/models/train.py
+++ src/models/train.py
@@ -22,8 +22,8 @@ def train_model(X_train, y_train):
     
     # Save the model in as a file
     # print("Loading...model file")
-    joblib.dump(model, "models/vinit_model.pkl")
-    # print("loaded.")
+    joblib.dump(best_model, "models/vinit_model.pkl")
+    print("loaded.")
 
     # print("Model Trained")
     return best_model, best_params

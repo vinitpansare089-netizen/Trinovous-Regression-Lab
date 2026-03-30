69f8a1e8cd0adf679569744632a8bfa473f9e07c
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-03-31 01:01:20 +0530

    added a log file

---
+++ src/logger.py
@@ -0,0 +1,13 @@
+import logging
+import os
+
+os.makedirs("artifacts", exist_ok=True)
+
+logging.basicConfig(
+    filename="artifacts/app.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+def get_logger():
+    return logging

f5e98da03710f3f528326f00a2828460e1c6f949
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-03-31 19:30:01 +0530

    Tested APIs

---
+++ app.py
@@ -0,0 +1,39 @@
+import joblib
+from fastapi import FastAPI
+import pandas as pd
+# from sklearn.preprocessing import MinMaxScaler
+
+#app banana hai
+app = FastAPI()
+
+##ML model ko load krenge
+model = joblib.load("models/vinit_model.pkl")
+scaler = joblib.load("models/vinit_scalar.pkl")
+columns = joblib.load("models/columns.pkl")
+
+@app.get("/")
+def Home():
+    return {'message' : "working Vinit...."}
+
+@app.post("/predict")
+def predict(data: dict):
+    print("incoming request: ", data)
+
+    #convert karega data frame me kyuki dict ke form me aa raha hai
+    df = pd.DataFrame([data])
+
+    #convert categorical columns to numerical columns...
+    df = pd.get_dummies(df)
+
+    #reindexing karne ke liye according to what we except
+    df = df.reindex(columns=columns, fill_value=0)
+
+    df = scaler.transform(df)
+
+    prediction = model.predict(df)[0]
+
+    return {f'Predictions' : float(prediction)}
+
+
+
+
---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+    res.send("Node working 🚀");
+});
+
+app.post("/predict", async (req, res) => {
+    try {
+        console.log("Incoming:", req.body);
+
+        const response = await axios.post(
+            "http://127.0.0.1:8000/predict",
+            req.body
+        );
+
+        res.json(response.data);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ error: "ML API error" });
+    }
+});
+
+app.listen(5000, () => {
+    console.log("Backend running on port 5000 🚀");
+});
+
+// app.get("/", (req, res) => {
+//     res.send("Node working 🚀");
+// });

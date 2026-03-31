37be5202cdeca5942ab20ea8e840afbb380bb3b1
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-03-31 12:35:13 +0530

    changes in documentation in preprocessing

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -39,7 +39,7 @@ def preprocess_data(df):
         X, y, test_size=0.2, random_state=42
     )
     
-    # Scaling
+    # Scaling the columns (0-1)
     scaler = StandardScaler()
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)

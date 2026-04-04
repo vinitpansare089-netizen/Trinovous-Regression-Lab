f6c8d6db82fffee8b2e971474905381b6ab5afac
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-04-05 01:50:15 +0530

    added changes in main.py

--- main.py
+++ main.py
@@ -23,54 +23,58 @@ print(f"Model Type: {type(model)}")
 
 
 #evaluation
-mse, rmse = evaluate_model(model, X_test, y_test)
+mse, rmse, mae, r2 = evaluate_model(model, X_test, y_test)
 
-train_mse, train_rmse = evaluate_model(model, X_train, y_train)
-test_mse, test_rmse = evaluate_model(model, X_test, y_test)
+train_mse, train_rmse, train_mae, train_r2 = evaluate_model(model, X_train, y_train)
+test_mse, test_rmse, test_mae, test_r2 = evaluate_model(model, X_test, y_test)
 
 print("Train RMSE:", train_rmse)
 print("Test RMSE:", test_rmse)
+print("Test R2:", test_r2)
+print("Test Mae: ", mae)
 
-print("Final MSE:", mse)
-print("RSME: ", rmse)
+# print("Final MSE:", mse)
+# print("RSME: ", rmse)
+# print("R2: ", r2)
+# print("Test Mae: ", mae)
 
-while True:
-    studytime = int(input("Enter studytime: "))
-    failures = int(input("Enter failures: "))
-    absences = int(input("Enter absences(1-5): "))
-    # school = (input("Enter school: "))
-    # sex = int(input("Enter sex: "))
+#while True:
+    # studytime = int(input("Enter studytime: "))
+    # failures = int(input("Enter failures: "))
+    # absences = int(input("Enter absences(1-5): "))
+    # # school = (input("Enter school: "))
+    # # sex = int(input("Enter sex: "))
  
-    sample = {
-        "studytime": studytime,
-        "failures": failures,
-        "absences": absences,
-        "school": "GP",
-        "sex": "M"
-    }
+    # sample = {
+    #     "studytime": studytime,
+    #     "failures": failures,
+    #     "absences": absences,
+    #     "school": "GP",
+    #     "sex": "M"
+    # }
 
-    result = predict_new(sample)
+    # result = predict_new(sample)
 
-    print("Predicted G3:", result)
+    # print("Predicted G3:", result)
 
-    os.makedirs("artifacts", exist_ok=True)
+    # os.makedirs("artifacts", exist_ok=True)
 
-    results = {
-        "train_rmse": train_rmse,
-        "test_rmse": test_rmse,
-        "mse": mse,
-        "best_params": params
-    }
+    # results = {
+    #     "train_rmse": train_rmse,
+    #     "test_rmse": test_rmse,
+    #     "mse": mse,
+    #     "best_params": params
+    # }
 
-    with open("artifacts/results.json", "w") as f:
-        json.dump(results, f, indent=4)
+    # with open("artifacts/results.json", "w") as f:
+    #     json.dump(results, f, indent=4)
 
-    print("Results saved to artifacts/results.json")
+    # print("Results saved to artifacts/results.json")
     
-    logger = get_logger()
+    # logger = get_logger()
 
-    logger.info("Pipeline started")
-    logger.info(f"Best Params: {params}")
-    logger.info(f"Train RMSE: {train_rmse}")
-    logger.info(f"Test RMSE: {test_rmse}")
+    # logger.info("Pipeline started")
+    # logger.info(f"Best Params: {params}")
+    # logger.info(f"Train RMSE: {train_rmse}")
+    # logger.info(f"Test RMSE: {test_rmse}")
 

d110cc8dbc493f76b0240cb236f114b19cbde9bd
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-04-04 23:27:14 +0530

    update logger function

--- src/logger.py
+++ src/logger.py
@@ -6,7 +6,8 @@ os.makedirs("artifacts", exist_ok=True)
 logging.basicConfig(
     filename="artifacts/app.log",
     level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s"
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    filemode='w'
 )
 
 def get_logger():

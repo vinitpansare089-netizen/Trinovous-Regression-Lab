cccac7370e1ef997c00646abe28ae18c3257dc87
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-04-10 19:12:57 +0530

    fixes errors

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -1,10 +1,26 @@
 ### data loading function
 
 import pandas as pd
-import seaborn as sns
+# import seaborn as sns
 
 def load_data(path):
-    # print("Loading data")
+    try:
+        print("loading data...")
+        df = pd.read_csv(path)
+        print("data loaded")
+        return df
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f'Invalid file: {e}')
+
+
+
+
+
+
+
+
+def load_data(path):
+    #print("Loading data")
     df = pd.read_csv(path)
     # print("Data Loaded")
     return df

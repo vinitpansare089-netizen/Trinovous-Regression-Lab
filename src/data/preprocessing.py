945c8e0e5e5e6c8404b16276807de7b984d6c392
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-04-11 23:44:57 +0530

    fixes errors

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -1,16 +1,4 @@
-## preprocessing done through this module
-# import pandas as pd
-# def preprocess(df):
-#     print("Preprossing data")
 
-#     #drop null values
-#     df = df.dropna()
-
-#     #convert categorical columns to numeric
-#     df = pd.get_dummies(df, drop_first=True)
-    
-#     print("preprossing done")
-#     return df
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
